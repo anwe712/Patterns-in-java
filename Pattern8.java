165dbe5c543c4fc4554b58a2611817e9878947ee
anwe712 <ghoshanwesha272@gmail.com>
2023-05-16 14:06:08 +0530

    code added

---
+++ Pattern8.java
@@ -0,0 +1,12 @@
+public class Pattern8 {
+    public static void main(String[] args) {
+        for(int i = 1 ; i <=5 ; i+=2)
+        {
+            for(int j = 1 ; j <= i ; j+=2)
+            {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
