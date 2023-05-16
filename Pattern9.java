dbd628dc5c683c2494503b0c74d6abcd473badd7
anwe712 <ghoshanwesha272@gmail.com>
2023-05-16 14:09:30 +0530

    code added

---
+++ Pattern9.java
@@ -0,0 +1,12 @@
+public class Pattern9 {
+    public static void main(String[] args) {
+        for (int i = 2 ; i <= 6 ; i+= 2)
+        {
+            for (int j = 1 ; j <= i ; j++)
+            {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
