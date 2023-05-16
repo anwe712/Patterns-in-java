7b792c760f30d6758beb0e574c4f1bf4610dc9bf
anwe712 <ghoshanwesha272@gmail.com>
2023-05-16 10:40:49 +0530

    changes done

--- HollowRectangle.java
+++ HollowRectangle.java
@@ -4,7 +4,8 @@ public class HollowRectangle {
         for (int i = 1 ; i <=5 ; i++ )
         {
             for(int j = 1 ; j <= 4 ; j++)
-            {
+            {   // Print asterisk (*) for the first and last row
+                // or for the first and last column
                 if(i==1 || j == 4 || i == 5 || j == 1)
                 {
                     System.out.print("*");
